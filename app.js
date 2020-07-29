d4bcb1e718148bcf4909bebb2bd326f32dbd6858
elikagowda <68097290+elikagowda@users.noreply.github.com>
2020-07-29 02:43:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const session = require('express-session');
+
+/** Configured Passport */
+const passport = require('./config/passport');
+
+/** Mongoose connection */
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/auth-example');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 /**
  * By default the sessions will be removed after restarting the server.
  * In order to have some sort of persistent session use the compatible
